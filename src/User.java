25c91384ecde93fbcc3333f6abe72529b74c8b50
Beryl-18 <beryla.s@hotmail.com>
2020-03-31 19:22:25 +0400

    Working on User Class

--- src/User.java
+++ src/User.java
@@ -1,8 +1,27 @@
-public class User {
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class User extends Application {
+    private MongoClient mongo = new MongoClient("Localhost", 27017);
+    private DB LibraryProject = mongo.getDB("LibraryProject");
+    private DBCollection UsersCollection = LibraryProject.getCollection("Users");
+
+    //Instances of the class
+    private double UserID;
+    private static String[] Username = new String[2];
+
     public void LogInUi(){
 
     }
     public void RegistrationUI(){
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
     }
 }
