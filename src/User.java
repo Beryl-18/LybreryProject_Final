d6ac1d480c53778d9b0596288ceee60e5f10000a
Beryl-18 <beryla.s@hotmail.com>
2020-03-31 23:06:05 +0400

    Working on User Class

--- src/User.java
+++ src/User.java
@@ -2,6 +2,10 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class User extends Application {
@@ -9,19 +13,36 @@ public class User extends Application {
     private DB LibraryProject = mongo.getDB("LibraryProject");
     private DBCollection UsersCollection = LibraryProject.getCollection("Users");
 
+    Stage UserFloorStage;
+
     //Instances of the class
-    private double UserID;
+    private double userID;
     private static String[] Username = new String[2];
 
-    public void LogInUi(){
+    //Login UI scene
+    public void logInUi(){
+        Scene loggingIn;
 
+        GridPane logInLayout = new GridPane();
+        logInLayout.setPadding(new Insets(15,7,15,7));
+        logInLayout.setVgap(6);logInLayout.setHgap(6);
+        Label userText = new Label("USERNAME:");
+        logInLayout.add(userText,1,3,2,0);
+
+        loggingIn = new Scene (logInLayout,300,300);
     }
-    public void RegistrationUI(){
+
+    //Registration UI Scene
+    public void registrationUI(){
 
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        UserFloorStage = primaryStage;
+
+        UserFloorStage.setTitle("Log In");
+        UserFloorStage.show();
 
     }
 }
